035c34e825008ee9cd20d4be0fc4afa683f386f2
Lorene Pecci <lorenepecci@gmail.com>
2022-06-12 09:55:18 -0300

    todos os models concluidos das tabelas

---
+++ src/database/models/Users.js
@@ -0,0 +1,19 @@
+const UsersSchema = ( sequelize, dataTypes ) => {
+  const Users = sequelize.define( "User", {
+    id:{ type: dataTypes.INTEGER, primaryKey: true, autoIncrement: true, },
+    displayName: dataTypes.STRING(255),
+    password: dataTypes.STRING(255),
+    email: dataTypes.STRING(255),
+    image: dataTypes.STRING(255),
+  }, {
+    timestamps: false,
+  } );
+
+  Users.associate = (models) => {
+    Users.hasMany(models.BlogPost, {as: 'blogPost', foreignKey: 'userId'});
+  }
+
+  return Users;
+}
+
+module.exports = UsersSchema;
